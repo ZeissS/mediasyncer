e66e988a3fc1029afd51b536b0d83e2ecf219cb2
Stephan Zeissler <stephan@giantswarm.io>
2015-09-30 23:40:15 +0200

    More text

---
+++ inmemory/volume.go
@@ -0,0 +1,41 @@
+package inmemory
+
+import "time"
+
+type File struct {
+	Path    string
+	Name    string
+	ModTime time.Time
+	Size    uint64
+	Dir     bool
+}
+
+type Volume struct {
+	id    string
+	Files map[string]File
+	Size  uint64
+}
+
+func NewVolume(id string, size uint64) *Volume {
+	return &Volume{
+		id:    id,
+		Files: []File{},
+		Size:  size,
+	}
+}
+
+func (v *Volume) ID() string {
+	return v.id
+}
+
+func (v *Volume) AvailableBytes() uint64 {
+	space := v.Size
+	for _, file := range v.Files {
+		space -= file.Size
+	}
+	return space
+}
+
+func (v *Volume) Walk(f filepath.WalkFunc) error {
+
+}
