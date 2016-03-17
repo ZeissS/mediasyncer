1cc6ed421899b899ac1fff6783f9a38b4ef18c10
Stephan Zeissler <stephan@giantswarm.io>
2016-03-18 00:16:31 +0100

    Added godoc for Bidder

--- libsyncer/bidder.go
+++ libsyncer/bidder.go
@@ -5,16 +5,22 @@ import (
 	"os"
 )
 
+// The Bidder is a service that subscribes to AuctionStarted events on the NetworkProtocol,
+// calculates a bid with the PriceFormula and responds with a Bid.
+// If not enough space is available on the Volume, the auction is ignored.
+// If the PriceFormula returns a negative price, the auction is ignored.
 type Bidder struct {
-	Volume       Volume
-	Network      NetworkProtocol
-	PriceFormula PriceFormula
-	FileServer   *FileServer
+	volume       Volume
+	network      NetworkProtocol
+	priceFormula PriceFormula
+	fileServer   *FileServer
 
 	active   bool
 	auctions chan bidderAuctionStarted
 }
 
+// bidderAuctionStarted represents an internal message which is generated for
+// new auction events.
 type bidderAuctionStarted struct {
 	peer  string
 	ID    AuctionID
@@ -22,12 +28,14 @@ type bidderAuctionStarted struct {
 	stats FileStats
 }
 
+// NewBidder creates a new Bidder for the given dependencies. The bidder is not started yet,
+// but immediately subscribes to the NetworkProtocols OnAuctionStart.
 func NewBidder(n NetworkProtocol, vol Volume, pf PriceFormula, fs *FileServer) *Bidder {
 	b := &Bidder{
-		Network:      n,
-		Volume:       vol,
-		PriceFormula: pf,
-		FileServer:   fs,
+		network:      n,
+		volume:       vol,
+		priceFormula: pf,
+		fileServer:   fs,
 
 		active:   true,
 		auctions: make(chan bidderAuctionStarted),
@@ -40,12 +48,14 @@ func NewBidder(n NetworkProtocol, vol Volume, pf PriceFormula, fs *FileServer) *
 	return b
 }
 
+// Serve runs the bidder loop by consuming an internal chan to react to new auctions.
+// Each auction is handled sequentialy.
 func (b *Bidder) Serve() {
 	for b.active {
 		select {
 		case auction := <-b.auctions:
 			log.Println("Received auction " + string(auction.ID) + " from " + auction.peer + " for file " + auction.file.String())
-			freeSpace := ByteSize(b.Volume.AvailableBytes())
+			freeSpace := ByteSize(b.volume.AvailableBytes())
 			if freeSpace < auction.stats.Size {
 				log.Println(auction.ID + ": not bidding - not enough space on volume.")
 				continue
@@ -57,7 +67,7 @@ func (b *Bidder) Serve() {
 				continue
 			}
 
-			_, err := b.Volume.Stat(auction.file.Path)
+			_, err := b.volume.Stat(auction.file.Path)
 			if err != nil {
 				if os.IsNotExist(err) {
 					// Only bid, if we don't have this file locally.
@@ -68,7 +78,7 @@ func (b *Bidder) Serve() {
 					if err != nil {
 						panic("Unable to create upload URL")
 					}
-					b.Network.AuctionBid(auction.peer, auction.ID, price, url)
+					b.network.AuctionBid(auction.peer, auction.ID, price, url)
 					continue
 				}
 				panic("Stat error: " + err.Error())
@@ -78,6 +88,7 @@ func (b *Bidder) Serve() {
 	}
 }
 
+// Stop sets an internal flag to stop the bidder loop in Bidder.Serve()
 func (b *Bidder) Stop() {
 	b.active = false
 }
